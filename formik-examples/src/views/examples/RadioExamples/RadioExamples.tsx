beda75fc81b9d857412d3c582b5e22106c8f626a
Andrej Badin <contact@andrejbadin.com>
2019-06-03 00:16:33 +0200

    Add radios example

--- formik-examples/src/views/examples/RadioExamples/RadioExamples.tsx
+++ formik-examples/src/views/examples/RadioExamples/RadioExamples.tsx
@@ -1,3 +1,42 @@
+import { Field, FieldProps, Formik } from 'formik';
 import React from 'react';
+import { PreBlock } from '../../../components/PreBlock';
+import { DRINKS } from './constants';
 
-export const RadioExamples: React.FC = () => <div>radio</div>;
+export const RadioExamples: React.FC = () => {
+  const noop = () => {};
+  const isChecked = (radioValue: string, storedValue: string) =>
+    radioValue === storedValue;
+  return (
+    <Formik initialValues={{ favouriteDrink: '' }} onSubmit={noop}>
+      <React.Fragment>
+        <div className="shadow p-4 mb-8 rounded-lg p-10 flex flex-col items-start">
+          <h1 className="text-xl font-bold">What is your favourite drink?</h1>
+          {DRINKS.map((tier, index) => (
+            <label key={index} className="my-2 mt-8 cursor-pointer">
+              <Field name="ageTier">
+                {({ field }: FieldProps) => (
+                  <input
+                    {...field}
+                    type="radio"
+                    name={field.name}
+                    checked={isChecked(tier.value, field.value)}
+                    onChange={() => field.onChange(field.name)(tier.value)}
+                  />
+                )}
+              </Field>
+              <span className="pl-2">{tier.label}</span>
+            </label>
+          ))}
+        </div>
+        <Field>
+          {({ field }: FieldProps<any>) => (
+            <PreBlock title="Stored Formik values">
+              {JSON.stringify(field.value, null, 2)}
+            </PreBlock>
+          )}
+        </Field>
+      </React.Fragment>
+    </Formik>
+  );
+};
---
+++ formik-examples/src/views/examples/RadioExamples/constants.ts
@@ -0,0 +1,14 @@
+export const DRINKS = [
+  {
+    label: 'Water',
+    value: 'water',
+  },
+  {
+    label: 'Pepsi',
+    value: 'pepsi',
+  },
+  {
+    label: 'Coca-Cola',
+    value: 'coca cola',
+  },
+];
